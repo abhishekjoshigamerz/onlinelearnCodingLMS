c860c5b18bcc800fba582809f7de6d51ec774de4
ABHISHEK\abhis <abhishekjoshicctian1@gmail.com>
2023-07-01 21:45:29 +0530

    db name changed
    db name code changed

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/adminPanel');
+mongoose.connect('mongodb://localhost/adminPanel');
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
